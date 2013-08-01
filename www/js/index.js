23644300e3df3d65c3c127f136718fcc773d8a28
Jared Wolinsky <jared.wolinsky@gmail.com>
2013-07-31 22:43:36 -0400

    use device's contacts to autocomplete "who were you with?" feature

--- www/js/index.js
+++ www/js/index.js
@@ -12,16 +12,28 @@ var app = {
         });
 
         $.when(jqmReady, pgReady).then(function () {
-            var places = localStorage.getItem("myPlaces");
-            if (!places) {
-                places = "";
+            var places;
+            var placesStr = localStorage.getItem("myPlaces");
+            if (!placesStr || !((places = JSON.parse(placesStr)) instanceof Array)) {
+                places = [];
                 localStorage.setItem("myPlaces", JSON.stringify(places));
             }
-            places = JSON.parse(places);
             for (var i = 0; i < places.length; i++) {
                 app.addPlaceListItem(places[i]);
             }
+
+            navigator.contacts.find(["name"], app.retrievedContacts, app.logError, {multiple : true});
+
+            $("#date").val(app.getToday());
         });
+    },
+
+    retrievedContacts : function (contacts) {
+        this.contacts = contacts;
+    },
+    
+    addFriend : function(contact){
+        $("#listFriends").append("<li><h1>" + contact.name + "</h1><a data-icon='delete'></a></li>");
     },
 
     addPlaceListItem : function (place) {
@@ -57,11 +69,25 @@ var app = {
 
     clearPlaceForm : function () {
         $("#placeName").val("");
-        $("#date").val("");
+        $("#date").val(app.getToday());
         $("#friends").text("");
         $.mobile.changePage("#main");
-    }
+    },
 
+
+    /****************************
+     * Util
+     ****************************/
+    getToday : function () {
+        var d = new Date();
+        var month = d.getMonth() + 1;
+        var day = d.getDate();
+        return d.getFullYear() + '-' + (month < 10 ? '0' : '') + month + '-' + (day < 10 ? '0' : '') + day;
+    },
+
+    logError : function (err) {
+        console.log(err);
+    }
 };
 
 app.initialize();
